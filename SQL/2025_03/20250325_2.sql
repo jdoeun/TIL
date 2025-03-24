732dbdd90f7384f01367b31b1dfb691ffa01aa04
Doeun Jeong <jde429@khu.ac.kr>
2025-03-25 00:38:29 +0900

    programmers: 진료과별 총 예약 횟수 출력하기

---
+++ SQL/2025_03/20250325_2.sql
@@ -0,0 +1,8 @@
+-- programmers: 진료과별 총 예약 횟수 출력하기
+
+SELECT MCDP_CD AS '진료과코드', COUNT(*) AS '5월예약건수'
+FROM APPOINTMENT
+WHERE YEAR(APNT_YMD) = 2022
+    AND MONTH(APNT_YMD) = 5
+GROUP BY MCDP_CD
+ORDER BY COUNT(*), MCDP_CD;
