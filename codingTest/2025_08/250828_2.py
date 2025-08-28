f6fc9c604ebc2ed7aa92cd00e2b6fda1e031e670
Doeun Jeong <jde429@khu.ac.kr>
2025-08-28 20:30:53 +0900

    programmers: 숫자의 표현

---
+++ codingTest/2025_08/250828_2.py
@@ -0,0 +1,16 @@
+# programmers: 숫자의 표현
+
+def solution(n):
+    answer = 0
+
+    for i in range(1, n + 1):
+        hap = 0
+        for j in range(i, n + 1):
+            hap += j
+
+            if hap == n:
+                answer += 1
+            elif hap > n:
+                break
+
+    return answer
