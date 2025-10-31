52ac4913775bbdfc546857262f3c84c9b8e7a9ab
Doeun Jeong <jde429@khu.ac.kr>
2025-10-31 13:40:23 +0900

    BOJ: 11722 가장 긴 감소하는 부분 수열

---
+++ codingTest/2025_10/week4/251031_3.py
@@ -0,0 +1,13 @@
+# BOJ: 11722 가장 긴 감소하는 부분 수열
+
+n = int(input())
+A = list(map(int, input().split()))
+
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if A[i] < A[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
