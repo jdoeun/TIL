1c5aa449527b68322305c4e8fec034b558b64766
Doeun Jeong <jde429@khu.ac.kr>
2025-12-01 21:07:00 +0900

    BOJ: 11718 그대로 출력하기

---
+++ codingTest/2025_12/week1/251201_1.py
@@ -0,0 +1,12 @@
+# BOJ: 11718 그대로 출력하기
+
+import sys
+
+for line in sys.stdin:
+    print(line, end='')
+
+# while True:
+#     try:
+#         print(input())
+#     except EOFError:
+#         break
